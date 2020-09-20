4abaf301369e97541f3d9ea7ca79cf3974406fe2
michaelshmichael <michaeldcjohnson@gmail.com>
2020-09-20 18:18:03 +0600

    added decimal function and backspace function

--- script.js
+++ script.js
@@ -14,34 +14,74 @@ let displayText = "";
 numbers.forEach(function(number){
     number.addEventListener("click", function(){
         if(operatorValue === "") {
-        num1 += number.textContent;
-        displayText = num1;
-        display.textContent = displayText;
+            num1 += number.textContent;
+            display.textContent = num1;
         } else {
-        num2 += number.textContent;
-        displayText = num1 + operatorValue + num2;
-        display.textContent = displayText;
-        }
+            num2 += number.textContent;
+            display.textContent = num1 + operatorValue + num2;
+        }   
     })
 })
 
-// undo.addEventListener("click", function(){
-//     //displayValue = display.textContent.split("");
-//     displayText = displayText.slice(0, -1);
-//     display.textContent = display.textContent.slice(0, -1);
-// })
+decimal.addEventListener("click", function(){
+    if(operatorValue === "") {
+        num1 += decimal.textContent;
+        display.textContent = num1;
+        decimal.disabled = true;
+    } else {
+        num2 += decimal.textContent;
+        display.textContent = num1 + operatorValue + num2;
+        decimal.disabled = true;
+    }   
+})
 
 operators.forEach(function(operator){
     operator.addEventListener("click", function(){
+        decimal.disabled = false;
         operatorValue = operator.textContent;
         display.textContent = num1 + operatorValue;
     })
 })
 
 equals.addEventListener("click", function(){
+    if(operatorValue === "" || num2 === ""){
+        alert("Invalid, please check your input")
+    } else {
     operate(operatorValue, num1, num2);
+    num1 = "";
+    num2 = "";
+    operatorValue = "";
+    decimal.disabled = false;
+    }
 })
 
+undo.addEventListener("click", function(){
+    if(num2 != "") {
+        num2 = num2.slice(0, -1);
+        display.textContent = num1 + operatorValue + num2;
+    } else if(operatorValue != ""){
+        operatorValue === "";
+        display.textContent = num1
+    } else if(num1 != ""){
+        num1 = num1.slice(0, -1);
+        display.textContent = num1;
+    }
+})
+
+function operate (operator, num1, num2) {
+    switch(operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            return "Invalid operation"
+    }
+}
 
 function add(num1, num2) {
     let total = parseFloat(num1) + parseFloat(num2);
@@ -68,22 +108,9 @@ function divide (num1, num2) {
    
 }
 
-function operate (operator, num1, num2) {
-    switch(operator) {
-        case "+":
-            return add(num1, num2);
-        case "-":
-            return subtract(num1, num2);
-        case "*":
-            return multiply(num1, num2);
-        case "/":
-            return divide(num1, num2);
-        default:
-            return "Invalid operation"
-    }
-}
 
 clear.addEventListener("click", function(){
+    decimal.disabled = false;
     display.textContent = "0";
     operatorValue = "";
     num1 = "";
